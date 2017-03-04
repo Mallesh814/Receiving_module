931097ec8da6a6cbbb4547e4dc25ff83843a9dfa
mallesh814 <mallesh.iisc@gmail.com>
2017-03-04 08:16:34 +0530

    LED Driving Using Timers
    Using Timers to generate 4KHz sampling pulses. Timer0 Configured to
    trigger Timer1. But its not working as of now. Added gitignore

---
+++ Debug/configs.d
@@ -0,0 +1,30 @@
+configs.o: ../configs.c ../configs.h \
+ /Users/mallesh/tivaware/inc/tm4c123gh6pm.h \
+ /Users/mallesh/tivaware/inc/hw_memmap.h \
+ /Users/mallesh/tivaware/inc/hw_types.h \
+ /Users/mallesh/tivaware/driverlib/gpio.h \
+ /Users/mallesh/tivaware/driverlib/pin_map.h \
+ /Users/mallesh/tivaware/driverlib/sysctl.h \
+ /Users/mallesh/tivaware/driverlib/uart.h \
+ /Users/mallesh/tivaware/driverlib/i2c.h \
+ /Users/mallesh/tivaware/driverlib/interrupt.h
+
+../configs.h:
+
+/Users/mallesh/tivaware/inc/tm4c123gh6pm.h:
+
+/Users/mallesh/tivaware/inc/hw_memmap.h:
+
+/Users/mallesh/tivaware/inc/hw_types.h:
+
+/Users/mallesh/tivaware/driverlib/gpio.h:
+
+/Users/mallesh/tivaware/driverlib/pin_map.h:
+
+/Users/mallesh/tivaware/driverlib/sysctl.h:
+
+/Users/mallesh/tivaware/driverlib/uart.h:
+
+/Users/mallesh/tivaware/driverlib/i2c.h:
+
+/Users/mallesh/tivaware/driverlib/interrupt.h:
